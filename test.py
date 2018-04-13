c2f3fe2bf6b41957548e79fd4328f8e1127d7ba8
ksh1ng <opompm123@yahoo.com.tw>
2018-04-13 20:31:00 +0800

    0.0

---
+++ test.py
@@ -0,0 +1,23 @@
+import turtle
+import random
+wn=turtle.Screen()
+wn.bgcolor('lightblue')
+nick=turtle.Turtle()
+benson=turtle.Turtle()
+
+nick.shape("turtle")
+benson.shape("turtle")
+
+nick.color("red")
+benson.fillcolor("red")
+
+nick.up()
+benson.up()
+nick.position(-300,20);benson.position(-300,-20)
+
+for i in range(40):
+    nick.forward(random.randrange(1,10));benson.forward(random.randrange(1,10))
+
+
+
+wn.exitonclick()
