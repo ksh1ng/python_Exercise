13eaa56c3bef9e518d3e04d612c00af52ba00c5a
ksh1ng <opompm123@yahoo.com.tw>
2018-04-13 23:12:47 +0800

    0.1

--- test.py
+++ test.py
@@ -9,12 +9,12 @@ nick.shape("turtle")
 benson.shape("turtle")
 
 nick.color("red")
-benson.fillcolor("red")
+benson.fillcolor("green")
 
 nick.up()
 benson.up()
 nick.position(-300,20);benson.position(-300,-20)
-
+nick.down()
 for i in range(40):
     nick.forward(random.randrange(1,10));benson.forward(random.randrange(1,10))
 
