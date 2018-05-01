5d63fbfcdf4070a6a412f33b76b7220064a69ac2
ksh1ng <opompm123@yahoo.com.tw>
2018-05-01 14:06:56 +0800

    check

---
+++ ws_0211.py
@@ -0,0 +1,29 @@
+'''Every exercise in worksheet'''
+def bluebird_count():
+    sum=0
+    num_watchers=int(input("Enter the number of teams:"))
+    for i  in range(num_watchers):
+        print("----Observer {} -----".format(i+1))
+        num_bird=int(input("Enter number spotted by this observer:"))
+        sum=sum+num_bird
+    #print("There were {} Eastern Bluebirds spotted today".format(sum))
+    return sum
+
+
+
+def  number_of_odds():
+    number=int(input("Enter a nonnegative integer:"))
+    num_odds=0
+    for n in range(number+1):
+        if n%2!=0:
+            num_odds=num_odds+1
+    print("The number of odds between 0 and",number,"is",num_odds)
+
+def main():
+    number_of_odds()
+
+    print("There were {} Eastern Bluebirds spotted today".format(bluebird_count()))
+
+
+if __name__ == '__main__':
+    main()
